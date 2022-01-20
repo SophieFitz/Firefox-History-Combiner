528c37f7336fc23f5c732b803581dda513b812c3
Josh Brenner <joshbrenner@protonmail.com>
2022-01-20 13:28:46 +0000

    Update deDuplicateDBs.py
    Move allVisits create statement so it balances out how long it appears to take to remove duplicate entries.
    
    Before, it looked like it took more time to start the program. Then it removed the entries instantly.
    
    Now, it should be more balanced.

--- programFiles/extra/deDuplicateDBs.py
+++ programFiles/extra/deDuplicateDBs.py
@@ -26,8 +26,6 @@ elif Path.cwd().joinpath('places.sqlite').is_file() == True:
     dbMain = sqlite3.connect('places.sqlite')
     curMain = dbMain.cursor()
 
-    allVisits = getAllEntries(cur = curMain, SQL ='SELECT * from main.moz_historyvisits order by id desc', dictSchema ='entry[0]: list(entry)')
-
     dupSQL = 'SELECT from_visit, place_id, visit_date, id, COUNT(*) Num FROM moz_historyvisits group by from_visit, place_id, visit_date having Num > 1'
     duplicateVisits = getAllEntries(cur = curMain, SQL = dupSQL, dictSchema ='(entry[0], entry[1], entry[2]): [entry[3], entry[4]]')
 
@@ -41,9 +39,11 @@ elif Path.cwd().joinpath('places.sqlite').is_file() == True:
         print(f'Removing {numDuplicates} duplicate entries...')
 
         done = []
+        allVisits = getAllEntries(cur = curMain, SQL ='SELECT * from main.moz_historyvisits order by id desc', dictSchema ='entry[0]: list(entry)')
+
         for visit in allVisits.values():
             if (visit[1], visit[2], visit[3]) in duplicateVisits.keys() and (visit[1], visit[2], visit[3]) not in done:
-                lowestID, numDuplicates = duplicateVisits[(visit[1], visit[2], visit[3])][:]
+                lowestID = duplicateVisits[(visit[1], visit[2], visit[3])][0]
                 curMain.execute('DELETE from main.moz_historyvisits where from_visit = ? and place_id = ? and visit_date = ? and id > ?', (visit[1], visit[2], visit[3], lowestID))
                 done.append((visit[1], visit[2], visit[3]))
 
